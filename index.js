3b0892ab206b8c22749d9bbf07748e207d57bf38
syn-zeta <syn-zeta@users.noreply.github.com>
2017-07-24 23:42:48 -0700

    Add option to specify globs to watch in watch mode

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ exports.init = function (filePath) {
 exports.watch = function (opts) {
   gulp.task('build', build(opts))
   exports.run(opts)
-  gulp.watch(opts.config || config.config, ['build'])
+  gulp.watch(config.watchGlobs || config.config, ['build'])
 }
 
 exports.run = function (opts, cb) {
